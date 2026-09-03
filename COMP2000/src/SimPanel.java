c7b4a0ff9004e75e7a4b4417e1a26f50a6ccd2b6
Shahriar Hasan <shahriar.hasan1924@gmail.com>
2026-09-03 16:47:51 +1000

    Refactor SimPanel to render entities from World

--- COMP2000/src/SimPanel.java
+++ COMP2000/src/SimPanel.java
@@ -1,23 +1,36 @@
 import javax.swing.*;
 import java.awt.*;
 import java.net.URL;
-import java.util.List;
+
 
 public class SimPanel extends JPanel {
-    private final List<Building> buildings;
+    private final World world;
     private final Image backgroundImage;
 
-    public SimPanel(List<Building> buildings) {
-        this.buildings = buildings;
-        this.setPreferredSize(new Dimension(1000, 750));
-        URL backgroundURL = SimPanel.class.getResource("/Background.png");
+    public SimPanel(World world) {
+        this.world = world;
+
+        setPreferredSize(
+                new Dimension(
+                        world.getWidth(),
+                        world.getHeight()
+                )
+        );
+
+        URL backgroundURL =
+                SimPanel.class.getResource("/Background.png");
+
         if (backgroundURL == null) {
-            throw new IllegalStateException("background.png was not found");
-        } else {
-            this.backgroundImage = (new ImageIcon(backgroundURL)).getImage();
+            throw new IllegalStateException(
+                    "Background.png was not found"
+            );
         }
+
+        this.backgroundImage =
+                new ImageIcon(backgroundURL).getImage();
     }
 
+    @Override
     protected void paintComponent(Graphics graphics) {
         super.paintComponent(graphics);
         Graphics2D graphics2D = (Graphics2D)graphics.create();
@@ -25,8 +38,10 @@ public class SimPanel extends JPanel {
         try {
             graphics2D.drawImage(this.backgroundImage, 0, 0, this.getWidth(), this.getHeight(), this);
 
-            for(Building building : this.buildings) {
-                building.draw(graphics2D);
+            for (Entity entity : world.getEntities()) {
+                if (entity.isActive()) {
+                    entity.draw(graphics2D);
+                }
             }
         } finally {
             graphics2D.dispose();
