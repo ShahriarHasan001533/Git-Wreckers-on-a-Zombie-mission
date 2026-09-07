17e9ad3ce4dcd92dede119dc4c74a22d0829499d
Khoi Nguyen Nguyen Tran <khoinguyennn231@gmail.com>
2026-09-08 01:39:33 +1000

    added simulation speed and reset controls

--- COMP2000/src/Main.java
+++ COMP2000/src/Main.java
@@ -10,25 +10,12 @@ public class Main {
         SwingUtilities.invokeLater(() -> {
 
             World world = new World(1000,750);
+            populateWorld(world);
 
-            Building leftBuilding = new Building(155, 125, 150, 100);
-            Building rightBuilding = new Building(580, 490,150, 100);
-            
-            world.addEntity(leftBuilding);
-            world.addEntity(rightBuilding);
-
-
-            SimPanel simPanel =
-                    new SimPanel(world);
-
-            // Part B: people wander until they find and consume nearby food.
-            world.addEntity(new Human(100, 400));
-            world.addEntity(new Human(450, 200));
-            world.addEntity(new Human(800, 350));
-
-            world.addEntity(new Food(250, 300, 45));
-            world.addEntity(new Food(500, 600, 45));
-            world.addEntity(new Food(850, 150, 45));
+            SimPanel simPanel = new SimPanel(world, () -> {
+                world.reset();
+                populateWorld(world);
+            });
 
             JFrame frame =
                     new JFrame("Zombie Simulation");
@@ -56,5 +43,19 @@ public class Main {
 
             timer.start();
         });
+    }
+
+    private static void populateWorld(World world) {
+        world.addEntity(new Building(155, 125, 150, 100));
+        world.addEntity(new Building(580, 490, 150, 100));
+
+        // Part B: people wander until they find and consume nearby food.
+        world.addEntity(new Human(100, 400));
+        world.addEntity(new Human(450, 200));
+        world.addEntity(new Human(800, 350));
+
+        world.addEntity(new Food(250, 300, 45));
+        world.addEntity(new Food(500, 600, 45));
+        world.addEntity(new Food(850, 150, 45));
     }
 }
